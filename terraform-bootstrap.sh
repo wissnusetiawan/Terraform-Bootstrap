5d8e9c7103644b1561e71ea4bc5e69d44c2dd60e
Wisnu Setiawan <wissnusetiawan@gmail.com>
2022-11-21 02:06:34 +0700

    Create terraform-bootstrap.sh

---
+++ terraform-bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+######################################################################
+# Script Name    : terraform-bootstrap.sh
+# Description    : Used to create a blob storage account for Terraform state files
+# Args           : RESOURCE_GROUP_NAME LOCATION STORAGE_ACCOUNT_NAME
+# Author         : Wisnu Setiawan <wissnusetiawan@gmail.com>
+######################################################################
